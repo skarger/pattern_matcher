bd6dc52592ba9817a278a4796a17d0067bc90ec1
Stephen Karger <stephen.karger@gmail.com>
2017-05-19 19:47:10 -0400

    implement wildcard pattern matcher

--- lib/pattern_matcher.rb
+++ lib/pattern_matcher.rb
@@ -1,5 +1,60 @@
 require "pattern_matcher/version"
 
 module PatternMatcher
-  # Your code goes here...
+  def self.match(input, pattern)
+    letters = input.split ""
+    self.match_list(letters, pattern)
+  end
+
+  private
+
+  def self.match_list(letters, pattern)
+    if pattern == ""
+      letters == []
+    else
+      initial_pattern, rest_of_pattern = partition_pattern(pattern)
+      pattern_letter = initial_pattern[0]
+
+      if zero_or_more_pattern? initial_pattern
+        lazy_match?(letters, pattern_letter, rest_of_pattern)
+      else
+        letters[0] == pattern_letter &&
+          match_list(letters[1, letters.length], rest_of_pattern)
+      end
+    end
+  end
+
+  # expects pattern to be a string with length >= 1
+  def self.partition_pattern(pattern)
+    if pattern.length > 1
+      if pattern[1] == "*"
+        # "a*bab*a" -> ["a*", "bab*a"]
+        [pattern.slice(0, 2), pattern.slice(2, pattern.length)]
+      else
+        # "abab*a" -> ["a", "bab*a"]
+        [pattern[0], pattern.slice(1, pattern.length)]
+      end
+    else
+      # "b" -> ["b", ""]
+      [pattern[0], ""]
+    end
+  end
+
+  def self.zero_or_more_pattern?(pattern)
+    pattern.length > 1
+  end
+
+  def self.lazy_match?(letters, pattern_letter, rest_of_pattern)
+    return true if letters == []
+
+    return match_list(letters, rest_of_pattern) if letters[0] != pattern_letter
+
+    letters.each_with_index do |letter, index|
+      rest_of_input = letters[index + 1, letters.length]
+      if match_list(rest_of_input, rest_of_pattern)
+        return true
+      end
+    end
+    return false
+  end
 end
--- spec/pattern_matcher_spec.rb
+++ spec/pattern_matcher_spec.rb
@@ -5,7 +5,89 @@ RSpec.describe PatternMatcher do
     expect(PatternMatcher::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "accepts an input string and a pattern" do
+    expect { PatternMatcher.match("", "") }.not_to raise_error
+  end
+
+  it "returns false for an empty pattern unless input is also empty" do
+    expect(PatternMatcher.match("a", "")).to eq(false)
+    expect(PatternMatcher.match("", "")).to eq(true)
+  end
+
+  context "patterns without * character" do
+    it "returns true for exact matches" do
+      expect(PatternMatcher.match("a", "a")).to eq(true)
+      expect(PatternMatcher.match("b", "b")).to eq(true)
+
+      expect(PatternMatcher.match("aaa", "aaa")).to eq(true)
+      expect(PatternMatcher.match("bbb", "bbb")).to eq(true)
+      expect(PatternMatcher.match("abab", "abab")).to eq(true)
+      expect(PatternMatcher.match("bab", "bab")).to eq(true)
+    end
+
+    it "returns false if pattern is longer or shorter than input" do
+      expect(PatternMatcher.match("ab", "aba")).to eq(false)
+      expect(PatternMatcher.match("bbaa", "baa")).to eq(false)
+    end
+
+    it "returns false if pattern sequence is different that input sequence" do
+      expect(PatternMatcher.match("a", "b")).to eq(false)
+      expect(PatternMatcher.match("aa", "ba")).to eq(false)
+      expect(PatternMatcher.match("aba", "abb")).to eq(false)
+    end
+
+    it "returns false if pattern is non-empty and input is empty" do
+      expect(PatternMatcher.match("", "b")).to eq(false)
+    end
+  end
+
+  context "patterns with * character" do
+    it "handles zero or more occurrences" do
+      expect(PatternMatcher.match("", "a*")).to eq(true)
+
+      expect(PatternMatcher.match("a", "a*")).to eq(true)
+      expect(PatternMatcher.match("aa", "a*")).to eq(true)
+      expect(PatternMatcher.match("aaa", "a*")).to eq(true)
+
+      expect(PatternMatcher.match("b", "b*")).to eq(true)
+      expect(PatternMatcher.match("bb", "b*")).to eq(true)
+      expect(PatternMatcher.match("bbb", "b*")).to eq(true)
+    end
+
+    it "handles a mix of pattern characters" do
+      expect(PatternMatcher.match("aab", "a*b")).to eq(true)
+      expect(PatternMatcher.match("abb", "ab*")).to eq(true)
+    end
+
+    it "handles zero occurrences followed by > 0 occurrences" do
+      expect(PatternMatcher.match("b", "a*b")).to eq(true)
+      expect(PatternMatcher.match("aa", "b*aa")).to eq(true)
+    end
+
+    it "returns false if *-repetition cannot satisfy match" do
+      expect(PatternMatcher.match("b", "a*")).to eq(false)
+      expect(PatternMatcher.match("bbab", "b*a")).to eq(false)
+      expect(PatternMatcher.match("bb", "b*a")).to eq(false)
+    end
+
+    it "still supports sub-runs with no * character" do
+      expect(PatternMatcher.match("baab", "baab*")).to eq(true)
+    end
+  end
+
+  context "patterns with multiple * characters" do
+    it "handles > 1 wildcard" do
+      expect(PatternMatcher.match("ab", "a*b*")).to eq(true)
+      expect(PatternMatcher.match("aba", "a*b*a*")).to eq(true)
+    end
+
+    it "handles a mix of wildcards and plain letter runs" do
+      expect(PatternMatcher.match("baabbbab", "ba*b*ab")).to eq(true)
+      expect(PatternMatcher.match("aabbbab", "aab*ab*")).to eq(true)
+    end
+
+    it "matches lazily, not greedily" do
+      expect(PatternMatcher.match("aabb", "a*ab*b")).to eq(true)
+    end
   end
 end
